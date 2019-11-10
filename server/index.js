17cbbccfd2ccc1ddc5a8bdc90508227899e64858
happydhKim <happytime870616@gmail.com>
2019-11-10 23:42:17 +0900

    [ FEATUER ] 그래프큐엘 테스트 (#2)
    Why
    - 그래프큐엘이 제대로 작동하는지 확인해야 함.
    
    What
    - 그래프큐엘 간단한 스키마 작성으로 테스트

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 import { GraphQLServer } from 'graphql-yoga';
+import resolvers from './schema/resolvers';
 
 const server = new GraphQLServer({
-
+  typeDefs: './schema/schema.graphql',
+  resolvers
 });
 
 server.start(() => console.log('GraphQL Server Start!'));
